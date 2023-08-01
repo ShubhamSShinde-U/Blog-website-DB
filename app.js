f184858e7377afb3d37cdbb0e2e56c5b30e2bfca
Shubham Shinde <90249532+ShubhamSShinde-U@users.noreply.github.com>
2023-08-01 18:29:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.get("/posts/:postName", function(req, res){
     })
 });
 
-let port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Server started on port 3000");
 });
